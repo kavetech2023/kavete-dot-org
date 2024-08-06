9c9404dce86676261096d8b51823462dc68785e5
kavetech2023 <kavetech@gmail.com>
2024-08-06 21:45:50 +0300

    chore: Update npm dependency for Vite to version 5.3.5 and make minor CSS adjustments in Hero component

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,8 +7,9 @@ const Hero = () => {
   return (
     <div className='hero container'>
         <div className="hero-text">
-            <h1>Giving Africa Opportunity.</h1>
+            <h1>Jobs for Africa.</h1>
             <p>Our cutting edge curriculum is designed to empower students with the knowledge, skills, and experiences needed to excel in the dynamic field of education.</p>
+            <input type="text" className='btn' placeholder="Search for Job" />
             <button className="btn">Explore more <span><FaRegCaretSquareRight /></span> </button>
         </div>
     </div>
